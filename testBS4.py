80acb3f86a7fbe9c86507d7cffd44de41de24e66
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2020-07-29 19:45:26 +0300

    Testing BeautifulSoup4

---
+++ testBS4.py
@@ -0,0 +1,10 @@
+import bs4
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.f1-fansite.com/f1-result/qualifying-results-2020-austrian-f1-gp/'
+page = urlopen(my_url)
+
+contents = soup(my_url, 'html.parser')
+
+print(contents)
