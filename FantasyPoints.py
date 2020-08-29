864cf7e8e840db279470499fa20cd628bc39ca62
SiddhantSingh15 <singhsiddhant@hotmail.co.uk>
2020-08-29 17:48:45 +0300

    Adding values for Q7

--- FantasyPoints.py
+++ FantasyPoints.py
@@ -33,6 +33,7 @@ class Driver:
             points += 11-self.qualiPos
 
         self.qualiPoints = points
+        print(self.name, ' - ', self.qualiPoints)
 
     def calculateRace(self):
         points = 0
@@ -147,7 +148,7 @@ VER.racePos = 2
 VER.finishRace = True
 VER.beatTeammate = True
 VER.fastestLap = False
-VER.streaks = 10
+VER.streaks = 0
 VER.calculateQuali()
 VER.calculateRace()
 VER.calculateTotal()
@@ -157,7 +158,7 @@ ALB = Driver()
 ALB.name = 'ALB'
 ALB.qualiSesh = 'Q3'
 ALB.beatTeammateQuali = False
-ALB.qualiPos = 6
+ALB.qualiPos = 5
 ALB.racePos = 8
 ALB.finishRace = True
 ALB.beatTeammate = False
@@ -172,12 +173,12 @@ STR = Driver()
 STR.name = 'STR'
 STR.qualiSesh = 'Q3'
 STR.beatTeammateQuali = False
-STR.qualiPos = 5
+STR.qualiPos = 9
 STR.racePos = 4
 STR.finishRace = True
 STR.beatTeammate = True
 STR.fastestLap = False
-STR.streaks = 10
+STR.streaks = 5
 STR.calculateQuali()
 STR.calculateRace()
 STR.calculateTotal()
@@ -187,7 +188,7 @@ PER = Driver()
 PER.name = 'PER'
 PER.qualiSesh = 'Q3'
 PER.beatTeammateQuali = True
-PER.qualiPos = 4
+PER.qualiPos = 8
 PER.racePos = 5
 PER.finishRace = True
 PER.beatTeammate = False
@@ -202,7 +203,7 @@ NOR = Driver()
 NOR.name = 'NOR'
 NOR.qualiSesh = 'Q3'
 NOR.beatTeammateQuali = False
-NOR.qualiPos = 8
+NOR.qualiPos = 10
 NOR.racePos = 10
 NOR.finishRace = True
 NOR.beatTeammate = False
@@ -232,7 +233,7 @@ VET = Driver()
 VET.name = 'VET'
 VET.qualiSesh = 'Q2'
 VET.beatTeammateQuali = False
-VET.qualiPos = 11
+VET.qualiPos = 14
 VET.racePos = 7
 VET.finishRace = True
 VET.beatTeammate = True
@@ -245,9 +246,9 @@ VET.calculateTotal()
 
 LEC = Driver()
 LEC.name = 'LEC'
-LEC.qualiSesh = 'Q3'
+LEC.qualiSesh = 'Q2'
 LEC.beatTeammateQuali = True
-LEC.qualiPos = 9
+LEC.qualiPos = 13
 LEC.DNF = True
 LEC.finishRace = False
 LEC.beatTeammate = False
@@ -260,9 +261,9 @@ LEC.calculateTotal()
 
 RUS = Driver()
 RUS.name = 'RUS'
-RUS.qualiSesh = 'Q1'
+RUS.qualiSesh = 'Q2'
 RUS.beatTeammateQuali = True
-RUS.qualiPos = 18
+RUS.qualiPos = 15
 RUS.racePos = 17
 RUS.finishRace = True
 RUS.beatTeammate = True
@@ -290,7 +291,7 @@ LAT.calculateTotal()
 
 RAI = Driver()
 RAI.name = 'RAI'
-RAI.qualiSesh = 'Q2'
+RAI.qualiSesh = 'Q1'
 RAI.beatTeammateQuali = True
 RAI.qualiPos = 14
 RAI.racePos = 14
@@ -307,7 +308,7 @@ GIO = Driver()
 GIO.name = 'GIO'
 GIO.qualiSesh = 'Q1'
 GIO.beatTeammateQuali = False
-GIO.qualiPos = 20
+GIO.qualiPos = 18
 GIO.racePos = 16
 GIO.finishRace = True
 GIO.beatTeammate = False
@@ -320,9 +321,9 @@ GIO.calculateTotal()
 
 GAS = Driver()
 GAS.name = 'GAS'
-GAS.qualiSesh = 'Q3'
-GAS.beatTeammateQuali = True
-GAS.qualiPos = 10
+GAS.qualiSesh = 'Q2'
+GAS.beatTeammateQuali = False
+GAS.qualiPos = 12
 GAS.racePos = 9
 GAS.finishRace = True
 GAS.beatTeammate = True
@@ -336,8 +337,8 @@ GAS.calculateTotal()
 KVY = Driver()
 KVY.name = 'KVY'
 KVY.qualiSesh = 'Q2'
-KVY.beatTeammateQuali = False
-KVY.qualiPos = 12
+KVY.beatTeammateQuali = True
+KVY.qualiPos = 11
 KVY.racePos = 12
 KVY.finishRace = True
 KVY.beatTeammate = False
@@ -350,9 +351,9 @@ KVY.calculateTotal()
 
 MAG = Driver()
 MAG.name = 'MAG'
-MAG.qualiSesh = 'Q2'
+MAG.qualiSesh = 'Q1'
 MAG.beatTeammateQuali = False
-MAG.qualiPos = 16
+MAG.qualiPos = 20
 MAG.racePos = 15
 MAG.finishRace = True
 MAG.beatTeammate = True
@@ -366,7 +367,7 @@ MAG.calculateTotal()
 GRO = Driver()
 GRO.name = 'GRO'
 GRO.qualiSesh = 'Q1'
-GRO.beatTeammateQuali = False
+GRO.beatTeammateQuali = True
 GRO.qualiPos = 17
 GRO.racePos = 19
 GRO.finishRace = True
@@ -380,9 +381,9 @@ GRO.calculateTotal()
 
 RIC = Driver()
 RIC.name = 'RIC'
-RIC.qualiSesh = 'Q2'
+RIC.qualiSesh = 'Q3'
 RIC.beatTeammateQuali = True
-RIC.qualiPos = 13
+RIC.qualiPos = 4
 RIC.racePos = 11
 RIC.finishRace = True
 RIC.beatTeammate = True
@@ -397,7 +398,7 @@ OCO = Driver()
 OCO.name = 'OCO'
 OCO.qualiSesh = 'Q2'
 OCO.beatTeammateQuali = False
-OCO.qualiPos = 15
+OCO.qualiPos = 6
 OCO.racePos = 13
 OCO.finishRace = True
 OCO.beatTeammate = False
